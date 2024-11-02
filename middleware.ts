ee8eaf4b88dfa44188b20ba820864a5afed48551
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-02 12:51:40 +0000

    authentication with clerk

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,32 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
+  const { userId, orgId } = await auth();
+  if (userId && isPublicRoute(request)) {
+    let path = "/select-org";
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+    const orgSelect = new URL(path, request.url);
+    return NextResponse.redirect(orgSelect);
   }
-})
-
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+  if (userId && !orgId && request.nextUrl.pathname !=="/select-org") {
+    const orgSelect = new URL(`/select-org`, request.url);
+    return NextResponse.redirect(orgSelect)
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
