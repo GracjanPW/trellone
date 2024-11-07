acde01ef10a818519771cf2438abe27323a2aea0
GracjanPW <40919569+GracjanPW@users.noreply.github.com>
2024-11-07 14:27:07 +0100

    card modal

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import CardModal from "@/components/modals/CardModal";
+import { useEffect, useState } from "react";
+
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
+  if (!isMounted) return null;
+  return (
+    <>
+      <CardModal/>
+    </>
+  )
+}
+
+export default ModalProvider
